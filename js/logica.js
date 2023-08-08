3ff6942468523fd70f0dab4fc0bc3088f5be93da
ainafetse <79762598+ainafetse@users.noreply.github.com>
2023-08-07 20:04:50 -0600

    Tercera pre entrega (events)

--- js/logica.js
+++ js/logica.js
@@ -1,7 +1,89 @@
-console.table(shirts);
-let productContainer = document.getElementById('search')
-let searchContainer = document.getElementById('search')
+console.table(products);
+const cart = [];
+let productContainer = document.getElementById('products')
+let cartItems = document.getElementById("cartItems");
 
+//DOM
+function productRendering(productList){
+    //we empty the container to avoid dupplicates
+    productContainer.innerHTML += "";
+    //Loading the cards of the requested products
+    for(const product of productList){
+        productContainer.innerHTML += `
+        <div class="card" style="width: 20rem;">
+            <img class="card-img-top" src=${product.photo} alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <p class="card-text">Precio $ ${product.price}.00</p>
+                <button id=${product.id} class="btn btn-outline-primary buy">Add to cart</button>
+            </div>
+        </div>
+        `;
+    }
+    //events
+    let buttons = document.getElementsByClassName("buy");
+    for (const button of buttons){
+        //opcion 1 - addEventListener
+        button.addEventListener('click',()=>{
+            console.log('there is a click on the button id: '+button.id);
+            const itemToCart = products.find((product) => product.id == button.id)
+            console.log(itemToCart);
+            addToCart(itemToCart);
+        })
+        //opcion 2 (hover)
+        button.onmouseover = () => button.classList.replace('btn-outline-primary', 'btn-primary');
+        button.onmouseout = () => button.classList.replace('btn-primary', 'btn-outline-primary');
+    }
+}
+
+productRendering(products);
+
+function addToCart(product){
+    cart.push(product);
+    console.table(cart);
+    cartItems.innerHTML += `
+    <tr>
+        <td>${product.id}</td>
+        <td><img src="${product.photo}" alt="${product.name}" class="cart-size"></td>
+        <td>${product.name}</td>
+        <td>$ ${product.price}.00</td>
+    </tr>
+    `
+}
+
+//eventos de teclado
+let emailNewsletter = document.getElementById('email');
+
+emailNewsletter.onkeyup = () => {
+    if(emailNewsletter.value.length < 13 || ((!emailNewsletter.value.includes('@')|| !emailNewsletter.value.includes('.')))){
+        console.log('invalid email')
+        emailNewsletter.style.color ='#bb0f0f';
+        document.getElementById('emailHelp').innerText = "Please enter a valid e-mail adress.";
+    }else{
+        emailNewsletter.style.color ='black';
+        document.getElementById('emailHelp').innerText = "Cool email! :)";
+    }
+}
+
+emailNewsletter.onchange = () => {
+    document.getElementById('emailHelp').innerText = "We'll never share your email with anyone else.";
+}
+
+let formNewsletter = document.getElementById('formNewsletter');
+formNewsletter.addEventListener('submit', validate);
+
+function validate(ev){
+    if(emailNewsletter.value == ''){
+        ev.preventDefault();
+        alert('Enter a valid email')
+    }
+}
+
+
+
+
+//let searchContainer = document.getElementById('search')
+/*
 function filterByPrice(maxPrice){
     const filtered = shirts.filter((shirt) => shirt.price <= maxPrice);
     console.log(filtered);
@@ -19,25 +101,9 @@ while(selectedPrice != 0){
         productRendering(filteredProducts);
     }
     selectedPrice = parseFloat(prompt('Precio máximo de prenda que buscas(0-exit)'));
-}
+}*/
 
-//DOM
-function productRendering(productList){
-    for(const shirt of productList){
-        productContainer.innerHTML += `
-        <div class="card" style="width: 20rem;">
-        <img class="card-img-top" src=${shirt.photo} alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">${shirt.name}</h5>
-          <p class="card-text">${shirt.price}</p>
-          <a href="#" class="btn btn-primary">Add to Cart</a>
-        </div>
-      </div>
-        `;
-    }
-}
-
-
+/*
 //Search by name filter
 function filterByName(name) {
     const result = shirts.filter(shirt => shirt.name.toLowerCase().includes(name.toLowerCase()));
@@ -46,12 +112,12 @@ function filterByName(name) {
 
 
 document.getElementById('btnSearch').addEventListener('click', function(event) {
-  event.preventDefault();
-  const nameSearch = document.getElementById('searchInput').value;
-  const searchProducts = filterByName(nameSearch);
-  console.table(searchProducts);
+    event.preventDefault();
+    const nameSearch = document.getElementById('searchInput').value;
+    const searchProducts = filterByName(nameSearch);
+    console.table(searchProducts);
   // Clear previous results and render the new search results
-  searchRendering(searchProducts);
+    searchRendering(searchProducts);
 });
 
 //DOM of searchbar
@@ -64,13 +130,14 @@ function searchRendering(searchProducts, clearPreviousResults = true) {
     for (const shirt of searchProducts) {
     searchContainer.innerHTML += `
         <div class="card" style="width: 20rem;">
-        <img class="card-img-top" src=${shirt.photo} alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">${shirt.name}</h5>
-            <p class="card-text">${shirt.price}</p>
-            <a href="#" class="btn btn-primary">Add to Cart</a>
-        </div>
+            <img class="card-img-top" src=${shirt.photo} alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${shirt.name}</h5>
+                <p class="card-text">Precio $ ${shirt.price}.00</p>
+                <a href="#" class="btn btn-primary">Add to Cart</a>
+            </div>
         </div>
     `;
     }
 }
+*/
--- js/productos.js
+++ js/productos.js
@@ -1,62 +1,150 @@
-const shirts = [{
+const products = [{
     id: 1,
     photo: "../assets/img/product-00-A.jpg",
     name: "BYNCE",
-    price: 899
+    price: 899,
+    category: "tshirts"
 },
 {
     id: 2,
     photo: "../assets/img/product-01-A.jpg",
     name: "FRDA",
-    price: 1200
+    price: 1200,
+    category: "tshirts"
 },
 {
     id: 3,
     photo: "../assets/img/product-02-A.jpg",
     name: "VRGN",
-    price: 899
+    price: 899,
+    category: "tshirts"
 },
 {
     id: 4,
     photo: "../assets/img/product-03-A.jpg",
     name: "DONNA",
-    price: 699
+    price: 699,
+    category: "tshirts"
 },
 {
     id: 5,
     photo: "../assets/img/product-04-A.jpg",
-    name: "DVD",
-    price: 899
+    name: "BLNT",
+    price: 899,
+    category: "tshirts"
 },
 {
     id: 6,
     photo: "../assets/img/product-05-A.jpg",
-    name: "DVID",
-    price: 899
+    name: "FMSX",
+    price: 899,
+    category: "tshirts"
 },
 {
     id: 7,
     photo: "../assets/img/product-06-A.jpg",
-    name: "MDSA",
-    price: 899
+    name: "ALN",
+    price: 899,
+    category: "tshirts"
 },
 {
     id: 8,
     photo: "../assets/img/product-07-A.jpg",
-    name: "ERRNG",
-    price: 899
+    name: "KSS",
+    price: 899,
+    category: "tshirts"
 },
 {
     id: 9,
     photo: "../assets/img/product-08-A.jpg",
-    name: "FRDAFX",
-    price: 1200
+    name: "RDNG",
+    price: 1200,
+    category: "tshirts"
 },
 {
     id: 10,
     photo: "../assets/img/product-09-A.jpg",
-    name: "VNGOH",
-    price: 1200
+    name: "FCKFF",
+    price: 1200,
+    category: "tshirts"
 },
+{
+    id: 11,
+    photo: "../assets/img/product-10-A.jpg",
+    name: "DNNA",
+    price: 1200,
+    category: "tshirts"
+},
+{
+    id: 12,
+    photo: "../assets/img/product-11-A.jpg",
+    name: "SMKNG",
+    price: 1200,
+    category: "tshirts"
+},
+{
+    id: 13,
+    photo: "../assets/img/product-12-A.jpg",
+    name: "ICCRM",
+    price: 1200,
+    category: "tshirts"
+},
+{
+    id: 14,
+    photo: "../assets/img/product-13-A.jpg",
+    name: "LLPOP",
+    price: 1200,
+    category: "tshirts"
+},
+{
+    id: 15,
+    photo: "../assets/img/product-14-A.jpg",
+    name: "MCRRN",
+    price: 1200,
+    category: "tshirts"
+},
+{
+    id: 16,
+    photo: "../assets/img/product-15-A.jpg",
+    name: "LNADR",
+    price: 1200,
+    category: "tshirts"
+},
+{
+    id: 17,
+    photo: "../assets/img/product-16-A.jpg",
+    name: "BYNC II",
+    price: 1200,
+    category: "tshirts"
+},
+{
+    id: 18,
+    photo: "../assets/img/product-17-A.jpg",
+    name: "ERRNG",
+    price: 1200,
+    category: "tshirts"
+},
+{
+    id: 19,
+    photo: "../assets/img/product-18-A.jpg",
+    name: "MCNGL",
+    price: 1200,
+    category: "tshirts"
+},
+{
+    id: 20,
+    photo: "../assets/img/product-19-A.jpg",
+    name: "FRD FX",
+    price: 1200,
+    category: "tshirts"
+},
+{
+    id: 21,
+    photo: "../assets/img/product-20-A.jpg",
+    name: "ERRNG SDNY",
+    price: 1200,
+    category: "tshirts"
+},
+
 
 ];
